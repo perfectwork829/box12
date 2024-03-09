228cd3466ed8aa11663590d20f649b74aadd36ba
perfectwork829 <superdev829@gmail.com>
2024-03-09 21:27:06 +0800

    fixed workflow

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: true,
+    api: 'https://memvera.com/api/',
+    domain: '178.128.21.124',
+    package_name: "com.memvera.boxtwelve",
+    front: 'http://178.128.21.124/',
+    mapKey: "AIzaSyC2rOSqCoBxZNrqM-zv7KIoKw2dsR2zCDc",    
+    paymentKey: "pk_live_pNvVb0tuoxiLEIYg5qDkGas"
+};
+//
+// export const environment = {
+//     production: true,
+//     api: 'https://memvera.com/api/',
+//     domain: 'box12ksa.com',
+//     package_name: "com.memvera.boxtwelve",
+//     front: 'http://www.box12ksa.com/',
+//     mapKey: "AIzaSyC2rOSqCoBxZNrqM-zv7KIoKw2dsR2zCDc",    
+//     paymentKey: "pk_live_pNvVb0tuoxiLEIYg5qDkGas"
+// };
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+	production: false,
+	api: 'https://memvera.com/api/',  
+  domain: 'box12ksa.com',  
+  package_name: "com.memvera.boxtwelve",
+	front: 'http://localhost:4202/',
+  mapKey: "AIzaSyC2rOSqCoBxZNrqM-zv7KIoKw2dsR2zCDc",
+  paymentKey: "pk_test_pYbcfzRvUuQrHNOqMemLKtkW"
+};
